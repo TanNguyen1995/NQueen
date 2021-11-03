33a9bda1c4d3b8b7b1650a0adf9380c1283dac1e
Nguyễn Minh Tân <93659806+TanNguyen1995@users.noreply.github.com>
2021-11-04 00:18:40 +0700

    Add files via upload

---
+++ NQueen.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class NQueen {
+    int row;
+    int col;
+    public NQueen(int row, int col){
+        super();
+        this.row = row;
+        this.col = col;
+    }
+}
